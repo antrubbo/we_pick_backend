5385e801a2cdacf2bbbdf99bbc3a2d5704225ccc
antrubbo <anthony.rubbo@gmail.com>
2021-02-04 10:58:02 -0500

    create user functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+    before_action :authenticate
+
+    def authenticate
+        @current_user = User.last
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,23 @@
 class UsersController < ApplicationController
-
     def index
         users = User.all 
         render json: users
     end
 
+    def create 
+        user = User.create(user_params)
+        if user.valid?
+            render json: user
+        else 
+            render json: {errors: user.errors.full_messages},
+            status: :not_acceptable
+        end
+    end
+
     private
 
     def user_params
+        params.permit(:username, :email)
     end
     
 end
