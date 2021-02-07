2cd70d8c321f0e289ac58e5eee9d170da6e0f7fe
antrubbo <anthony.rubbo@gmail.com>
2021-02-06 21:02:35 -0500

    finished fetch to backend

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
+require 'themoviedb-api'
+require 'json'
+require 'rest-client'
+Tmdb::Api.key("99fdd78beedc847a99f420187e092842")
+
 class MoviesController < ApplicationController
-    Tmdb::Api.key("99fdd78beedc847a99f420187e092842")
 
     def index
         movies = Movie.all 
@@ -13,8 +17,8 @@ class MoviesController < ApplicationController
 
     def details
         movie = Movie.find_by(id: params[:id])
-        # movie_resp = Tmdb::Movie.detail(movie.search_id)
-        render json: movie
+        movie_deets = RestClient.get("https://api.themoviedb.org/3/movie/#{movie.search_id}?api_key=99fdd78beedc847a99f420187e092842&language=en-US&append_to_response=videos,recommendations")
+        render json: movie_deets
     end
 
     private
