cef313131e47e3d9bdd1fecf8c1e8ce0c9ee7c43
antrubbo <anthony.rubbo@gmail.com>
2021-02-16 15:18:22 -0500

    minor change to user rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
     has_many :lists, dependent: :destroy
     has_many :movie_choices, through: :lists
+    
+    validates :username, :email, presence: true
+    validates :email, uniqueness:
+    {message: 'An account associated with %{value} already exists'}
 
     after_create :create_list
     before_update :validate_update
-
-    validates :username, :email, presence: true
-    validates :email, uniqueness:
-        {message: 'An account associated with %{value} already exists'}
 
     private
 
